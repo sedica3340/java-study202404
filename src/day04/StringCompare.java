c46062824d21892e489a378f88bfc9fd2e0abecf
sedica3340 <sedica3340@gmail.com>
2024-04-11 12:33:39 +0900

    Changes

---
+++ src/day04/StringCompare.java
@@ -0,0 +1,22 @@
+package day04;
+
+import java.util.Scanner;
+
+public class StringCompare {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String inputName = scan.nextLine();
+
+        String myName = new String("홍길동");
+        System.out.println("myName = " + myName);
+        System.out.println("inputName = " + inputName);
+
+//        char[] nickName = {'축','구','감','자'};
+//        String nick = new String(nickName);
+//        System.out.println(nick);
+
+        System.out.println(myName.equals(inputName) ? "두 문자열이 같다" : "두 문자열이 다르다");
+    }
+}
