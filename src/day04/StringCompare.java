b3e2fa7018daae265fdba3f8b0c13f1149d98102
sedica3340 <sedica3340@gmail.com>
2024-04-12 09:03:46 +0900

    Changes

--- src/day04/StringCompare.java
+++ src/day04/StringCompare.java
@@ -13,9 +13,11 @@ public class StringCompare {
         System.out.println("myName = " + myName);
         System.out.println("inputName = " + inputName);
 
-//        char[] nickName = {'&#xCD95;','&#xAD6C;','&#xAC10;','&#xC790;'};
-//        String nick = new String(nickName);
-//        System.out.println(nick);
+/*
+        char[] nickName = {'&#xCD95;','&#xAD6C;','&#xAC10;','&#xC790;'};
+        String nick = new String(nickName);
+        System.out.println(nick);
+*/
 
         System.out.println(myName.equals(inputName) ? "두 문자열이 같다" : "두 문자열이 다르다");
     }
