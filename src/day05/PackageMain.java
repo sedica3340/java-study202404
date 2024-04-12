34edf76b3d93088e579d5e401b97a5013342c16c
sedica3340 <sedica3340@gmail.com>
2024-04-12 10:51:40 +0900

    Changes

---
+++ src/day05/PackageMain.java
@@ -0,0 +1,18 @@
+package day05;
+
+//import day05.juice.Apple;
+//import day05.juice.Banana;
+//import day05.juice.Peach;
+import day05.juice.*;
+
+public class PackageMain {
+    public static void main(String[] args) {
+
+        day05.fruit.Apple fApple = new day05.fruit.Apple();
+        Apple jApple = new Apple();
+        new Banana();
+        new Peach();
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+    }
+}
---
+++ src/day05/juice/Apple.java
@@ -0,0 +1,8 @@
+package day05.juice;
+
+public class Apple {
+
+    public Apple() {
+        System.out.println("juice패키지의 사과입니다.");
+    }
+}
