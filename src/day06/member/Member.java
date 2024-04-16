cfae6a95d32931b1c1bc13fc431be24469be545b
sedica3340 <sedica3340@gmail.com>
2024-04-16 09:57:51 +0900

    Changes

---
+++ src/day06/member/Member.java
@@ -0,0 +1,31 @@
+package day06.member;
+
+import java.time.LocalDate;
+
+public class Member {
+
+    int id;
+    String email;
+    String memberName;
+    String password;
+    String gender;
+    int age ;
+    LocalDate regDate;
+
+    Member(String email, String password, String memberName, String gender, int age) {
+        this.id = 1;
+        this.regDate = LocalDate.now();
+
+        this.email = email;
+        this.password = email;
+        this.memberName = memberName;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String toString() {
+        return String.format("# 이름: %s, 이메일: %s, 나이: %d세, 성별: %s 가입일: %s"
+        , this.memberName, this.email, this. age, this.gender, this.regDate);
+
+    }
+}
