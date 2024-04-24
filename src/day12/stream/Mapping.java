57638feb4c3f27ff22fce9a3476bea8e0bc0554d
sedica3340 <sedica3340@gmail.com>
2024-04-24 17:10:20 +0900

    Changes

--- src/day12/stream/Mapping.java
+++ src/day12/stream/Mapping.java
@@ -44,5 +44,29 @@ public class Mapping {
                 .map(w -> w.charAt(0))
                 .collect(Collectors.toList());
         System.out.println(collect);
+
+        // 메뉴 목록에서 메뉴이름과 칼로리를 추출해서 새로운 객체에 포장하고 싶음
+
+        List<nameCalories> nameCaloriesList = Menu
+                .menuList
+                .stream()
+                .map(dish -> new nameCalories(dish))
+                .collect(Collectors.toList());
+
+        nameCaloriesList.forEach(nc -> System.out.println(nc));
+    }
+    public static class nameCalories {
+        private final String name;
+        private final int calories;
+
+        public nameCalories(String name, int calories) {
+            this.name = name;
+            this.calories = calories;
+        }
+
+        public nameCalories(Dish dish) {
+            this.name = dish.getName();
+            this.calories = dish.getCalories();
+        }
     }
 }
